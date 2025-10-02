e0d107ad46746959478bf76aa7752ac172b38d3b
Leonardo Eitner <leo.eitner@gmail.com>
2025-10-02 12:59:33 +0200

    remove unecessary  files cgpops

--- cgpops/cgpopsMeshHistoryMaxErrHDdata0.m
+++
@@ -1,8 +0,0 @@
-%% cgpopsMeshHistoryMaxErrHDdata0.m
-
-
-cgpopsMeshHistoryMaxErrHD(1,1) = 3.4674814865118516487207858e-06;
-cgpopsMeshHistoryMaxErrHD(2,1) = 6.4910939763796494335253715e-07;
-cgpopsMeshHistoryMaxErrHD(3,1) = 1.1059165961128461557303360e-07;
-cgpopsMeshHistoryMaxErrHD(4,1) = 9.7850728572482080021733165e-08;
-
--- cgpops/cgpopsMeshHistoryPhasePtsHDdata0.m
+++
@@ -1,8 +0,0 @@
-%% cgpopsMeshHistoryPhasePtsHDdata0.m
-
-
-cgpopsMeshHistoryPhasePtsHD(1,1) = 200;
-cgpopsMeshHistoryPhasePtsHD(2,1) = 226;
-cgpopsMeshHistoryPhasePtsHD(3,1) = 233;
-cgpopsMeshHistoryPhasePtsHD(4,1) = 234;
-
